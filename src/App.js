a7a9a57b6dc0c7267d3d62021d2aec69be34617b
Vish1811 <codevish1811@gmail.com>
2020-08-14 11:49:39 +0530

    Added the Sidebar Frontend

--- src/App.js
+++ src/App.js
@@ -6,7 +6,7 @@ import Sidebar from "./Sidebar";
 function App() {
   return (
     <div className="App">
-    <h1>Hello Vishnu programmer..........Let's build the Slack Clone</h1>
+    
     {/* Header */}
     <Header/>
     <div className="app__body">
--- src/Sidebar.js
+++ src/Sidebar.js
@@ -1,9 +1,48 @@
 import React from 'react';
 import "./Sidebar.css";
+import FiberManualRecordIcon from "@material-ui/icons/FiberManualRecordRounded"
+import CreateIcon from "@material-ui/icons/Create";
+import InsertCommentIcon from "@material-ui/icons/InsertComment";
+import SidebarOption from './SidebarOption';
+import InboxIcon from "@material-ui/icons/Inbox";
+import DraftsIcon from "@material-ui/icons/Drafts";
+import BookMarkBorderIcon from "@material-ui/icons/BookmarkBorder";
+import FileCopyIcon from "@material-ui/icons/FileCopy";
+import PeopleAltIcon from "@material-ui/icons/PeopleAlt";
+import AppsIcon from "@material-ui/icons/Apps";
+import ExpandLessicon from "@material-ui/icons/ExpandLess";
+import ExpandMoreicon from "@material-ui/icons/ExpandMore";
+import AddIcon from "@material-ui/icons/Add";
 
 function Sidebar() {
     return (
         <div className="sidebar">
+            <div className="sidebar__header">
+                <div className="sidebar__info">
+                    <h2> Vishnu Programmer</h2>
+                    <h3> 
+                        <FiberManualRecordIcon/>
+                        Vishnu Kumar
+
+                    </h3>
+
+                </div>
+            <CreateIcon/>
+            </div>
+            <SidebarOption Icon={InsertCommentIcon} title="Threads"/>
+            <SidebarOption Icon={InboxIcon} title="Mentions & Reactions"/>
+            <SidebarOption Icon={DraftsIcon} title="Saved items"/>
+            <SidebarOption Icon={BookMarkBorderIcon} title="Channel browser"/>
+            <SidebarOption Icon={PeopleAltIcon} title="People & user groups"/>
+            <SidebarOption Icon={AppsIcon} title="Apps"/>
+            <SidebarOption Icon={FileCopyIcon} title="File browser"/>
+            <SidebarOption Icon={ExpandLessicon} title="Show less"/>
+            <hr/>
+            <SidebarOption Icon={ExpandMoreicon} title="Channels"/>
+            <hr/>
+            <SidebarOption Icon={AddIcon} addChannelOption title="Add Channel"/>
+            {/*Connect to Db */}
+            {/*<SidebarOption/> ...............*/}
             
         </div>
     )
